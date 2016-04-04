bce042b61e29386ab49d4022cf61c9ba5f234edb
Kusmayadi <kusmayadi@gmail.com>
2016-04-04 13:12:23 +0700

    Fixed redis script

--- run/redis.sh
+++ run/redis.sh
@@ -6,7 +6,7 @@ case $CMD in
   "start" ) launchctl load /usr/local/opt/redis/homebrew.mxcl.redis.plist ;;
   "stop" ) launchctl unload /usr/local/opt/redis/homebrew.mxcl.redis.plist ;;
   "restart" ) 
-      launchctl unload /usr/local/opt/redis/homebrew.mxcl.redis.plist ;;
+      launchctl unload /usr/local/opt/redis/homebrew.mxcl.redis.plist
       launchctl load /usr/local/opt/redis/homebrew.mxcl.redis.plist ;;
   * ) echo "Usage: redish.sh start | stop | restart"
 esac
